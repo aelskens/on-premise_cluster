77a031ca507f6b5e187f14983a8b8f9ab2b7f728
aelskens <arthurelsk@gmail.com>
2024-12-16 15:19:44 +0100

    fix: nvidia container toolkit repo fix

--- setup_node.sh
+++ setup_node.sh
@@ -95,6 +95,11 @@ if ! $noinstall; then
     # Pop-OS has its own way to prioritize the package repo to install from, this allows to install the latest nvidia version
     # cf. https://github.com/NVIDIA/nvidia-container-toolkit/issues/23#issuecomment-1149806160
     sudo cp "$(dirname $0)/pop-os_nvidia-repo_fix"  /etc/apt/preferences.d/nvidia-docker-pin-1002
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+      sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+      sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    sudo sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
     sudo apt-get update
     sudo apt-get install -y nvidia-container-toolkit
     sudo nvidia-ctk runtime configure --runtime=docker --set-as-default
