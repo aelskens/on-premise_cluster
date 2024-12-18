c4493dc19112d388d14c99be1a91be84dad439e9
aelskens <arthurelsk@gmail.com>
2024-12-18 14:52:49 +0100

    refactor: rename variables to avoid confusion

--- join_node.sh
+++ join_node.sh
@@ -44,22 +44,22 @@ done
 # Main program.                                            #
 ############################################################
 
-read -p "User: " username
-read -p "Hostname: " hostname
+read -p "User: " remoteusername
+read -p "Hostname: " remotehostname
 
 # Get the join command
 OUTPUT="sudo $(sudo kubeadm token create --print-join-command)"
 
 # Execute the join command on the remote node through ssh
-ssh -tt $username@$hostname "eval $OUTPUT"
+ssh -tt $remoteusername@$remotehostname "eval $OUTPUT"
 
 # Add labels to node for GPU support
 # cf. https://github.com/NVIDIA/k8s-device-plugin/blob/main/deployments/helm/nvidia-device-plugin/values.yaml#L68
 if $gpu; then
   # On discrete-GPU based systems NFD adds the following label where 10de is the NVIDIA PCI vendor ID
-  kubectl label nodes $hostname feature.node.kubernetes.io/pci-10de.present=true
+  kubectl label nodes $remotehostname feature.node.kubernetes.io/pci-10de.present=true
   # On some Tegra-based systems NFD detects the CPU vendor ID as NVIDIA
-  kubectl label nodes $hostname feature.node.kubernetes.io/cpu-model.vendor_id="NVIDIA"
+  kubectl label nodes $remotehostname feature.node.kubernetes.io/cpu-model.vendor_id="NVIDIA"
   # We allow a GPU deployment to be forced by setting the following label to "true"
-  kubectl label nodes $hostname nvidia.com/gpu.present=true
+  kubectl label nodes $remotehostname nvidia.com/gpu.present=true
 fi
